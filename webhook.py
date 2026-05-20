c5b5850062a838ea67c6a641a2aa350f694119c8
Zephro3333 <zephro3333@gmail.com>
2026-05-20 09:39:15 +0100

    webhook.py

--- webhook.py
+++ webhook.py
@@ -1,22 +1,25 @@
 import os
 import requests
 
+
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
+print("🔥 WEBHOOK:", WEBHOOK_URL)
 
-def send_message(content: str):
-    print("🔥 SEND MESSAGE TRIGGERED")
 
+def send_message(content):
     if not WEBHOOK_URL:
-        print("❌ WEBHOOK_URL missing")
+        print("❌ WEBHOOK_URL NÃO EXISTE")
         return
 
     try:
-        r = requests.post(WEBHOOK_URL, json={"content": content})
-        print("STATUS:", r.status_code)
+        response = requests.post(
+            WEBHOOK_URL,
+            json={"content": content}
+        )
 
-        if r.status_code != 204:
-            print("ERROR:", r.text)
+        print("🔥 STATUS CODE:", response.status_code)
+        print("🔥 RESPONSE:", response.text)
 
     except Exception as e:
-        print("ERROR:", str(e))
+        print("❌ WEBHOOK ERROR:", str(e))
