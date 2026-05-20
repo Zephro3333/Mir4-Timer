8b142defb7df1bfc0eb57caae597e6c4005eb483
Zephro3333 <zephro3333@gmail.com>
2026-05-20 09:13:59 +0100

    discord_formatter.py

---
+++ discord_formatter.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+
+
+def build_boss_message(bosses):
+    if not bosses:
+        return ""
+
+    grouped = defaultdict(list)
+
+    for b in bosses:
+        grouped[b["time"]].append(b)
+
+    msg = "🔥 MIR4 BOSS ALERT\n\n"
+
+    for time in sorted(grouped.keys()):
+        msg += f"🕒 SERVER TIME: {time}\n\n"
+
+        for b in grouped[time]:
+            msg += (
+                f"🌍 World: {b['world']}\n"
+                f"🏷️ Layer: {b['layer']}\n"
+                f"⚔️ Boss: {b['boss']}\n"
+                f"📍 Location: {b['location']}\n"
+                "----------------------\n"
+            )
+
+        msg += "\n"
+
+    return msg
